d989f5d2350482c73b4edd6a9ab6a2e3c404dc18
alienwizard <johanwendin@gmail.com>
2016-08-09 08:12:55 -0400

    initial commit

---
+++ wp-content/plugins/wordfence/tmp/configCache.php
@@ -0,0 +1,4 @@
+<?php
+/* Wordfence temporary file security header */
+echo "Nothing to see here!\n"; exit(0);
+?>a:20:{s:6:"apiKey";s:0:"";s:9:"howGetIPs";s:0:"";s:11:"whitelisted";s:0:"";s:9:"cacheType";s:0:"";s:25:"ajaxWatcherDisabled_front";s:0:"";s:25:"ajaxWatcherDisabled_admin";s:0:"";s:20:"loginSec_lockoutMins";s:0:"";s:19:"other_hideWPVersion";s:0:"";s:26:"loginSec_disableAuthorScan";s:0:"";s:33:"scansEnabled_suspiciousAdminUsers";s:0:"";s:18:"liveTrafficEnabled";s:0:"";s:6:"isPaid";s:0:"";s:15:"firewallEnabled";s:0:"";s:18:"liveTraf_ignoreIPs";s:0:"";s:17:"liveTraf_ignoreUA";s:0:"";s:11:"other_WFNet";s:0:"";s:21:"scheduledScansEnabled";s:0:"";s:21:"betaThreatDefenseFeed";s:0:"";s:7:"debugOn";s:0:"";s:10:"ssl_verify";s:0:"";}
