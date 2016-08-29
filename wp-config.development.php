fa60a74b432ae306f642854ebe348debc2f2101e
alienwizard <johanwendin@gmail.com>
2016-08-29 05:41:12 -0400

    working on db error

--- wp-config.development.php
+++ wp-config.development.php
@@ -28,6 +28,10 @@ define('DB_HOST', 'localhost');
 define('DB_CHARSET', 'utf8');
 
 
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+
 define('WP_HOME', 'http://localhost:8080/tmsab');
 define('WP_SITEURL', 'http://localhost:8080/tmsab');
 
