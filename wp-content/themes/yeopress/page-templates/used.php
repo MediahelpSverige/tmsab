f9f8454bbc37350539542bbc701a877d0e356b99
alienwizard <johanwendin@gmail.com>
2016-08-30 08:41:05 +0200

    workin on css and other stuff

--- wp-content/themes/yeopress/page-templates/used.php
+++ wp-content/themes/yeopress/page-templates/used.php
@@ -23,15 +23,78 @@ get_header();
 <div class="col-md-9">
 	<div class="box-wrap">
 
-<?php the_title('<h1></h1>'); ?>
-	<?php the_content(); ?>
+<h3 class="title-border"><?php the_title(); ?></h3>
+		<?php the_content(); ?>
 </div>
 
 </div>
 
-</div>
+
+
+	<?php endwhile; else : ?>
+	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
 
 <div class="col-md-3">
+	<div class="box-wrap">
+	<h3 class="title-border">Kontakta en säljare idag</h3>
+
+
+<?php $args = array(
+	'post_type' => 'worker', 
+	'posts_per_page' => 3 ,
+	'tax_query' => array( 
+		array(
+			'taxonomy' => 'uppdrag',
+			'field' => 'slug',
+			'terms' => 'saljare'
+			),
+		),
+
+	); ?>
+
+
+<?php $the_query = new WP_Query( $args ); 
+
+if ( $the_query->have_posts() ) { ?>
+
+<div class="worker-wrap">
+
+<?php while ( $the_query->have_posts() ) { ?>
+
+
+
+	<?php $the_query->the_post(); ?>
+
+
+
+	
+	<div class="worker">
+
+
+
+		<img src="<?php the_post_thumbnail_url();?>">
+			<h3><?php the_title();?></h3>
+			<?php the_field('info'); ?>
+
+
+	</div>
+
+
+	<?php }
+
+
+	wp_reset_postdata();
+
+
+	}else{
+
+		//no posts
+
+		} ?>
+
+</div>
+		<div class="clearfix"></div>
 
 </div>
 			
@@ -39,9 +102,10 @@ get_header();
 
 </div>
 
-	<?php endwhile; else : ?>
-	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-<?php endif; ?>
+
+</div>
+
+
 
 <?php 
 
