4d7f06ccc658e4d271b5001deaf20e0d2df4c9f3
alienwizard <johanwendin@gmail.com>
2016-09-07 09:57:11 -0400

    added images

---
+++ wp-content/uploads/sucuri/sucuri-blockedusers.php
@@ -0,0 +1,6 @@
+<?php
+// datastore=blockedusers;
+// created_on=1473061562;
+// updated_on=1473061562;
+exit(0);
+?>
---
+++ wp-content/uploads/sucuri/sucuri-lastlogins.php
@@ -0,0 +1,4 @@
+<?php exit(0); ?>
+{"user_id":1,"user_login":"JohanMediahelp","user_remoteaddr":"195.178.187.202","user_hostname":"195.178.187.202","user_lastlogin":"2016-09-05 09:46:02"}
+{"user_id":2,"user_login":"david","user_remoteaddr":"81.235.159.148","user_hostname":"81-235-159-148-no29.tbcn.telia.com","user_lastlogin":"2016-09-07 11:16:39"}
+{"user_id":2,"user_login":"david","user_remoteaddr":"81.235.159.148","user_hostname":"81-235-159-148-no29.tbcn.telia.com","user_lastlogin":"2016-09-07 11:16:42"}
